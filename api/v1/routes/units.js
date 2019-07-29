7d55fd561b8ca212c3e520372d361846e00c4f3f
Jana Uhrich <janamu@uw.edu>
2019-07-28 17:18:46 -0700

    Delete company

--- api/v1/routes/units.js
+++ api/v1/routes/units.js
@@ -23,35 +23,29 @@ router.get("/units", async (req, res, next) => {
   res.json({ status, response });
 });
 
-//Update the unit with whatever information is specified in the request body and return the newly updated document.If the ID provided does not match a unit, return a 404 and an appropriate message.Note: You should allow for the company to be added from this route, if provided.
-
 //PATCH
 // http://localhost:5000/api/v1/units/5d36b6459505d2086326b425
 // got some inspiration on this from  https://dev.to/aurelkurtula/building-a-restful-api-with-express-and-mongodb--3mmh
 router.patch("/units/:id", async (req, res, next) => {
-
   try {
     let status = 200;
-    const findOneAndUpdate = await Units.findById(
-      req.params.id,
-      (err, unit) => {
-        if (err) { 
-          console.log(err)
-          return
-        }
-        if (req.body._id) {
-          //don't let them update the id, that'd be weird
-          delete req.body._id;
-        }
-        for (let update in req.body) {
-          //loop through each object and update if it changed
-          unit[update] = req.body[update];
-        }
-        //I still don't totally get when you need to .save() some things and when you don't but all the examples I looked at had this so it's probably important
-        unit.save();
+    const findByID = await Units.findById(req.params.id, (err, unit) => {
+      if (err) {
+        console.log(err);
+        return;
       }
-    );
-    //this seems to be running before findOneAndUpdate? 
+      if (req.body._id) {
+        //don't let them update the id, that'd be weird
+        delete req.body._id;
+      }
+      for (let update in req.body) {
+        //loop through each object and update if it changed
+        unit[update] = req.body[update];
+      }
+      //I still don't totally get when you need to .save() some things and when you don't but all the examples I looked at had this so it's probably important
+      unit.save();
+    });
+    //this seems to be running before findOneAndUpdate?
     const response = await Units.findById(req.params.id);
     res.json({ status, response });
   } catch (error) {
@@ -61,26 +55,58 @@ router.patch("/units/:id", async (req, res, next) => {
   }
 });
 
+//Patch
+//http://localhost:5000/api/v1/units/5d36b6750e7ed60948570285/company
+
 router.patch("/units/:id/company", async (req, res, next) => {
   const status = 200;
   try {
-    const findOneAndUpdate = await Units.findById(req.params.id, (err, unit) => {
-      if (err) { 
-        console.log(err)
-        return
+    const findOneAndUpdate = await Units.findById(
+      req.params.id,
+      (err, unit) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        for (let update in req.body) {
+          unit[update] = req.body[update];
+        }
+        unit.save();
       }
-      for (let update in req.body) {
-        unit[update] = req.body[update];
-      }
-      unit.save();
-    });
+    );
     const response = await Units.findById(req.params.id);
     res.json({ status, response });
-  }  catch (error) {
+  } catch (error) {
     error.status = 404;
     error.message = "Unit not found";
     next(error);
   }
+});
+
+// DELETE
+// http://localhost:5000/api/v1/units/5d36b6750e7ed60948570285/company
+
+router.delete("/units/:id/company", async (req, res, next) => {
+  const status = 200;
+   try {
+     const findAndDelete = Units.findById(
+       req.params.id,
+       (err, unit) => {
+         if (err) {
+           console.log(err);
+           return;
+         }
+         unit.company = [];
+         unit.save();
+       }
+     );
+     const response = await Units.findById(req.params.id);
+     res.json({ status, response });
+   } catch (error) {
+     error.status = 404;
+     error.message = "Unit not found";
+     next(error);
+   }
 });
 
 router.post("/units", async (req, res, next) => {
